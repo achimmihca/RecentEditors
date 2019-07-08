60d6e74558622ebaf95b638e482066f0be7be161
Andreas Stange <anst.dev@gmail.com>
2019-07-08 13:17:32 +0200

    created test project

---
+++ de.achimmihca.recenteditors.tests/src/de/achimmihca/recenteditors/tests/DummyTest.java
@@ -0,0 +1,12 @@
+package de.achimmihca.recenteditors.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummyTest() {
+		System.out.println( "testDummyTest" );
+	}
+
+}
